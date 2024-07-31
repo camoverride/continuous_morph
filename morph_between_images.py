66bd8b5fe9e80ed74fd51275bc7e60cbd4212a50
cam <cam@cams-MacBook-Pro.local>
2024-07-31 12:37:53 -0700

    ChatGPT - purge PIL

--- morph_between_images.py
+++ morph_between_images.py
@@ -1,10 +1,8 @@
 import numpy as np
 import cv2
 import os
-from PIL import Image
 import scipy.ndimage
 import dlib
-import PIL.Image
 
 # Apply affine transform calculated using srcTri and dstTri to src and output an image of size.
 def apply_affine_transform(src, srcTri, dstTri, size):
@@ -62,17 +60,15 @@ def generate_morph_sequence(duration, frame_rate, img1, img2, points1, points2, 
             morph_triangle(img1, img2, morphed_frame, t1, t2, t, alpha)
 
         print("writing image")
-        res = Image.fromarray(cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB))
+        res = cv2.cvtColor(np.uint8(morphed_frame), cv2.COLOR_BGR2RGB)
 
-        print("saving imae")
-        res.save(os.path.join(output_dir, f"frame_{j:04d}.jpg"), 'JPEG')
+        print("saving image")
+        cv2.imwrite(os.path.join(output_dir, f"frame_{j:04d}.jpg"), res)
         print("saved!")
         print("------")
 
-def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_size=4096, enable_padding=True, x_scale=1, y_scale=1, em_scale=0.1, alpha=False):
-    # Align function from FFHQ dataset pre-processing step
-    # https://github.com/NVlabs/ffhq-dataset/blob/master/download_ffhq.py
 
+def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_size=4096, enable_padding=True, x_scale=1, y_scale=1, em_scale=0.1, alpha=False):
     lm = np.array(face_landmarks)
     lm_chin          = lm[0  : 17]  # left-right
     lm_eyebrow_left  = lm[17 : 22]  # left-right
@@ -84,7 +80,6 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
     lm_mouth_outer   = lm[48 : 60]  # left-clockwise
     lm_mouth_inner   = lm[60 : 68]  # left-clockwise
 
-    # Calculate auxiliary vectors.
     eye_left     = np.mean(lm_eye_left, axis=0)
     eye_right    = np.mean(lm_eye_right, axis=0)
     eye_avg      = (eye_left + eye_right) * 0.5
@@ -94,7 +89,6 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
     mouth_avg    = (mouth_left + mouth_right) * 0.5
     eye_to_mouth = mouth_avg - eye_avg
 
-    # Choose oriented crop rectangle.
     x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
     x /= np.hypot(*x)
     x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
@@ -104,34 +98,26 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
     quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
     qsize = np.hypot(*x) * 2
 
-    # Load in-the-wild image.
-    if not os.path.isfile(src_file):
-        print('\nCannot find source image. Please run "--wilds" before "--align".')
-        return
-    img = PIL.Image.open(src_file).convert('RGBA').convert('RGB')
+    img = cv2.imread(src_file)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Shrink.
     shrink = int(np.floor(qsize / output_size * 0.5))
     if shrink > 1:
-        rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
-        img = img.resize(rsize, PIL.Image.LANCZOS)
+        rsize = (int(np.rint(img.shape[1] / shrink)), int(np.rint(img.shape[0] / shrink)))
+        img = cv2.resize(img, rsize, interpolation=cv2.INTER_LANCZOS4)
         quad /= shrink
         qsize /= shrink
 
-    # Crop.
     border = max(int(np.rint(qsize * 0.1)), 3)
     crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
-    crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
-    if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
-        img = img.crop(crop)
-        quad -= crop[0:2]
+    crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.shape[1]), min(crop[3] + border, img.shape[0]))
+    img = img[crop[1]:crop[3], crop[0]:crop[2]]
 
-    # Pad.
     pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
-    pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
+    pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.shape[1] + border, 0), max(pad[3] - img.shape[0] + border, 0))
     if enable_padding and max(pad) > border - 4:
         pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
-        img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+        img = np.pad(img, ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
         h, w, _ = img.shape
         y, x, _ = np.ogrid[:h, :w, :1]
         mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
@@ -139,22 +125,13 @@ def image_align(src_file, dst_file, face_landmarks, output_size=1024, transform_
         img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
         img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
         img = np.uint8(np.clip(np.rint(img), 0, 255))
-        if alpha:
-            mask = 1-np.clip(3.0 * mask, 0.0, 1.0)
-            mask = np.uint8(np.clip(np.rint(mask*255), 0, 255))
-            img = np.concatenate((img, mask), axis=2)
-            img = PIL.Image.fromarray(img, 'RGBA')
-        else:
-            img = PIL.Image.fromarray(img, 'RGB')
         quad += pad[:2]
 
-    # Transform.
-    img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+    img = cv2.warpPerspective(img, cv2.getPerspectiveTransform(np.array([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]], dtype='float32'), quad), (transform_size, transform_size), flags=cv2.INTER_LINEAR)
     if output_size < transform_size:
-        img = img.resize((output_size, output_size), PIL.Image.LANCZOS)
+        img = cv2.resize(img, (output_size, output_size), interpolation=cv2.INTER_LANCZOS4)
 
-    # Save aligned image.
-    img.save(dst_file, 'PNG')
+    cv2.imwrite(dst_file, img)
 
     return img
 
@@ -415,14 +392,10 @@ img2 = image_align("img2.jpg", "img2_output.png", landmarks_2, output_size=1024)
 # img1 = cv2.resize(np.array(img1), (320, 240))
 # img2 = cv2.resize(np.array(img2), (320, 240))
 print("resizing img 1")
-img1 = np.array(img1)
-img1 = img1.astype(np.uint8)
-img1 = cv2.resize(np.array(img1), (1500, 1500))
+img1 = cv2.resize(np.array(img1), (1000, 1000))
 
 print("resizing img 2")
-img2 = np.array(img2)
-img2 = img2.astype(np.uint8)
-img2 = cv2.resize(np.array(img2), (1500, 1500))
+img2 = cv2.resize(np.array(img2), (1000, 1000))
 
 # points1 = [(50, 50), (200, 50), (125, 200)]
 # points2 = [(60, 60), (210, 60), (135, 210)]
